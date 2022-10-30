641c05bf2c0f246a2672897db214437ad26114e5
Mikko Solismaa <mikko.solismaa@spatineo.com>
2022-10-30 14:23:45 +0200

    fix: testing

--- docker/image1/docker_command_publish.sh
+++ docker/image1/docker_command_publish.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-
+git config user.name github-actions
+git config user.email mikko.solismaa@spatineo.com
 poetry install --only publish
-.venv/bin/semantic-release --verbosity=INFO publish
+poetry run semantic-release --verbosity=INFO publish

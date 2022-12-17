849d683730ccf495311ca54c9a22b33b40537b34
Mikko Solismaa <mikko.solismaa@spatineo.com>
2022-12-17 15:29:36 +0200

    test

--- docker/image1/docker_command_publish.sh
+++ docker/image1/docker_command_publish.sh
@@ -3,4 +3,4 @@ git config user.name ${GITHUB_ACTOR:?}
 git config user.email mikko.solismaa@spatineo.com
 export GH_TOKEN=${GITHUB_TOKEN:?}
 poetry install --only publish
-poetry run semantic-release --verbosity=INFO publish
+# poetry run semantic-release --verbosity=INFO publish
